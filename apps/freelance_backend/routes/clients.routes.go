4335450f6cdfa49ca6313b2c0a68d7f380b93bb3
tanmay <kuamrchaurasiatanmay@gmail.com>
2025-02-13 04:26:45 +0530

    controller struct ready

--- apps/freelance_backend/routes/clients.routes.go
+++ apps/freelance_backend/routes/clients.routes.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	controller "golang-freelance_backend/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func ClientsRoutes(incommingRoutes *gin.Engine) {
-	incommingRoutes.POST("/create", controllers.generateClient())
+	incommingRoutes.POST("/create", controller.GenerateClient())
 }
